487ba22abbe088bf96c68b236bab030dfff9c040
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2024-11-18 00:23:35 +0100

    app change, note ex add

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,3 @@
-import Guitars from '../guitars/Guitars'
 import Sidenav from '../sidenav/Sidenav'
 import ValidHTML from '../valid-html/ValidHTML'
 import './App.scss'
@@ -11,7 +10,6 @@ function App() {
       <main className={s.main}>
         <div className={s.card}>
           <ValidHTML />
-          <Guitars />
         </div>
       </main>
     </>
